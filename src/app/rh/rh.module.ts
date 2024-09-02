ee666b5250893ed1df834eb8fce1f1f898525874
FABIAN ALEXANDER GARCIA MARTINEZ <fagarciam@sena.edu.co>
2024-09-02 18:22:39 -0500

    Base de estructura

---
+++ src/app/rh/rh.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RhRoutingModule } from './rh-routing.module';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { ListEmployeesComponent } from './pages/list-employees/list-employees.component';
+import { NewEmployeeComponent } from './pages/new-employee/new-employee.component';
+import { SearchEmployeeComponent } from './pages/search-employee/search-employee.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+    ListEmployeesComponent,
+    NewEmployeeComponent,
+    SearchEmployeeComponent,
+    EmployeeComponent
+  ],
+  imports: [
+    CommonModule,
+    RhRoutingModule
+  ]
+})
+export class RhModule { }
