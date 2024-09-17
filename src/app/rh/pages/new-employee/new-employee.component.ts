33ded4e8f8897cab18fd87967b676fcbef9a1b59
FABIAN ALEXANDER GARCIA MARTINEZ <fagarciam@sena.edu.co>
2024-09-17 15:24:36 -0500

    Configuracion de la plantilla del formulario desde TS

--- src/app/rh/pages/new-employee/new-employee.component.ts
+++ src/app/rh/pages/new-employee/new-employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartamentService } from '../../services/departament.service';
 import { Department } from '../../interfaces/departament.interface';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Department } from './../../interfaces/departament.interface';
 
 
 @Component({
@@ -17,9 +19,22 @@ export class NewEmployeeComponent implements OnInit {
   ngOnInit(): void {
       this.departamentService.getDepartamentos()
       .subscribe(rta => this.departaments = rta);
-
-
   }
 
+  public formEmployee = new FormGroup({
+    idEmpleadonew:    new FormControl(),
+    nombreEmpleado:   new FormControl<string>(''),
+    departamento:     new FormControl(this.departaments),
+    sueldoEmpleado:   new FormControl<number>(0),
+    imagenUrl:        new FormControl<string>(''),
+    descripcion:      new FormControl<string>(''),
+    habilidades:      new FormControl<string>(''),
+    imgAlternative:   new FormControl<string>('')
+  });
+
+  onSumit(){
+    console.log(this.formEmployee.value);
+
+  }
 
 }
